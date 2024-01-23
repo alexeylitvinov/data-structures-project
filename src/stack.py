e05fe365d079d724ef66590f6902be0fc9ee228b
alexeylitvinov <alextwin1976@gmail.com>
2024-01-23 15:59:39 +0100

    made homework-1

--- src/stack.py
+++ src/stack.py
@@ -1,21 +1,23 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
     """Класс для стека"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Конструктор класса Stack"""
-        pass
+        self.steck = []
+        self.top = None
 
     def push(self, data):
         """
@@ -23,12 +25,19 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data)
+        new_node.next_node = self.top
+        self.top = new_node
 
-    def pop(self):
+    def pop(self) -> None:
         """
         Метод для удаления элемента с вершины стека и его возвращения
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next_node
+            return popped_node.data
